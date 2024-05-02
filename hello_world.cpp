e8d89a745595919a4e97c0ddb77a7a2b6059ce65
VakulinGrigory <github.vakulingd@mail.ru>
2024-05-02 16:40:28 +0300

    changed code style in hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
-int main(){
-std::string username; // Переменная для имени пользователя
-std::cout << "Введите имя: "; std::cin >> username; // Ввод имени пользователя
-std::cout << "Hello world from " << username << "!" << std::endl;
-return 0;
+int
+main()
+{
+  std::string username; // Переменная для имени пользователя
+  std::cout << "Введите имя: ";
+  std::cin >> username; // Ввод имени пользователя
+  std::cout << "Hello world from " << username << "!" << std::endl;
+  return 0;
 }
