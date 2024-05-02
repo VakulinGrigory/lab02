552276556c4f4f35b40db59c1327ce45d191ff86
VakulinGrigory <github.vakulingd@mail.ru>
2024-05-02 16:26:56 +0300

    added comments to hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 int main(){
-std::string username;
-std::cout << "Введите имя: "; std::cin >> username;
+std::string username; // Переменная для имени пользователя
+std::cout << "Введите имя: "; std::cin >> username; // Ввод имени пользователя
 std::cout << "Hello world from " << username << "!" << std::endl;
 return 0;
 }
