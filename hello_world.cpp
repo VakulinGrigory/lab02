504b7b14064bd5d804c16d4af3ef092d5ded3a51
VakulinGrigory <github.vakulingd@mail.ru>
2024-05-02 16:19:17 +0300

    deleted using namespace std from hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
-using namespace std;
 int main(){
-string username;
-cout << "Введите имя: "; cin >> username;
-cout << "Hello world from " << username << "!" << endl;
+std::string username;
+std::cout << "Введите имя: "; std::cin >> username;
+std::cout << "Hello world from " << username << "!" << std::endl;
 return 0;
 }
